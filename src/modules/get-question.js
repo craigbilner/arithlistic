14d205d4eb13879ef3b63584e26924f518a16a68
Craig Bilner <craig.bilner@gmail.com>
2016-07-31 11:32:09 +0100

    Return questions

--- src/modules/get-question.js
+++ src/modules/get-question.js
@@ -1,4 +1,4 @@
-const questions = require('../lists');
+const questionLists = require('../lists');
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
@@ -6,7 +6,13 @@ const difficultyCount = {
   0: 2,
   1: 2,
   2: 3,
-  3: 4
+  3: 4,
+};
+
+const difficultyLimit = {
+  0: 5,
+  1: 10,
+  2: 20,
 };
 
 const generateLists = (listNames, count, lists = []) => {
@@ -19,8 +25,22 @@ const generateLists = (listNames, count, lists = []) => {
   return generateLists(listNames, count - 1, lists.concat(listNames[listIndx]));
 };
 
-module.exports = (difficulty = 0) => {
-  const lists = generateLists(Object.keys(questions), difficultyCount[difficulty]);
+const generateQuestions = (_questionLists, difficulty) => list => {
+  const questions = Object.keys(_questionLists[list]);
+  const maxIndx = Math.min(difficultyLimit[difficulty], questions.length);
+  const randomQuestionIndx = randomNumber(0, maxIndx);
+  const question = questions[randomQuestionIndx];
 
-  console.log(lists);
+  return {
+    list,
+    question,
+    answer: _questionLists[list][question],
+  };
+};
+
+module.exports = (difficulty = 0) => {
+  const lists = generateLists(Object.keys(questionLists), difficultyCount[difficulty]);
+  const questions = lists.map(generateQuestions(questionLists, difficulty));
+
+  console.log(questions);
 };
