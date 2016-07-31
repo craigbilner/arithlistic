10eac15d6c22bd2f6f2782504232231539493923
Craig Bilner <craig.bilner@gmail.com>
2016-07-31 15:19:54 +0100

    Add question logic

--- src/modules/get-question.js
+++ src/modules/get-question.js
@@ -15,6 +15,26 @@ const difficultyLimit = {
   2: 20,
 };
 
+const questionPrefixes = {
+  'beaufort scale': questionPart => questionPart === 'calm' ? '' : 'a ',
+  'mohs scale': () => '',
+  'herculean labour': () => 'the number of the herculean labour where he ',
+  'morse code': () => '',
+  'wedding gift': () => 'the number of years for a ',
+  presidents: () => '',
+  'periodic table': () => 'the atomic number of ',
+};
+
+const questionSuffixes = {
+  'beaufort scale': () => ' on the beaufort scale',
+  'mohs scale': () => ' on the mohs scale',
+  'herculean labour': () => '',
+  'morse code': () => ' in morse code',
+  'wedding gift': () => ' wedding anniversary',
+  presidents: () => '\'s presidency',
+  'periodic table': () => '',
+};
+
 const generateLists = (listNames, count, lists = []) => {
   if (count === 0) {
     return lists;
@@ -29,12 +49,15 @@ const generateQuestions = (_questionLists, difficulty) => list => {
   const questions = Object.keys(_questionLists[list]);
   const maxIndx = Math.min(difficultyLimit[difficulty], questions.length);
   const randomQuestionIndx = randomNumber(0, maxIndx);
-  const question = questions[randomQuestionIndx];
+  const questionPart = questions[randomQuestionIndx];
+  const prefix = questionPrefixes[list](questionPart);
+  const suffix = questionSuffixes[list](questionPart);
+  const question = `${prefix}${questionPart}${suffix}`;
 
   return {
     list,
     question,
-    answer: _questionLists[list][question],
+    answer: _questionLists[list][questionPart],
   };
 };
 
