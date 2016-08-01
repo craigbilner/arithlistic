ddb6e187bffeee207ed9102f86ff55166435b86e
Craig Bilner <craig.bilner@gmail.com>
2016-08-02 00:55:01 +0100

    Fix up tests

--- src/tests/all-the-tests.tests.js
+++ src/tests/all-the-tests.tests.js
@@ -71,33 +71,33 @@ describe('Alexa, start game', () => {
         it('Save name, start the game and ask Inigo Montoya the first question', () =>
           runIntent(nameIntent)
             .then(({ outputSpeech, gameState, names, startTime, currentAnswer }) => {
-              assert.deepEqual(outputSpeech.split(',')[0], 'Inigo Montoya');
+              assert.deepEqual(outputSpeech, 'Inigo Montoya, what is dash dash dash dash dash in ' +
+                'morse code, minus, the number of years, for a cotton wedding anniversary?');
               assert.deepEqual(gameState, GAME_STATES.PLAYING);
               assert.deepEqual(names[0], 'Inigo Montoya');
               assert.deepEqual(startTime, '2016-07-31T00:06:26Z');
-              assert(currentAnswer !== undefined);
+              assert.deepEqual(currentAnswer, -1);
             }));
 
         describe('The answer is five', () => {
           it('Score game, say answer is wrong and ask the next question', () =>
             runIntent(firstAnswerIntent)
               .then(({ outputSpeech, gameState, scores }) => {
-                const regex = /Incorrect, the answer was 6, you score, (.*) points. What is(.*)\?/;
-                assert(outputSpeech.match(regex).length);
+                assert.deepEqual(outputSpeech, 'Incorrect, the answer was 6, you score, ' +
+                  '46 points. What is the atomic number of, hydrogen, plus, ' +
+                  'George Washington\'s presidency?');
                 assert.deepEqual(gameState, GAME_STATES.PLAYING);
-                assert.deepEqual(scores.length, 1, 'has one score');
-                assert(Number.isFinite(scores[0]), 'score is a number');
+                assert.deepEqual(scores, [46]);
               }));
 
           describe('The answer is ten', () => {
             it('Score game, say answer is right and ask the next question', () =>
               runIntent(secondAnswerIntent)
                 .then(({ outputSpeech, gameState, scores }) => {
-                  const regex = /Correct for (.*) points. What is(.*)\?/;
-                  assert(outputSpeech.match(regex).length);
+                  assert.deepEqual(outputSpeech, 'Correct for 286 points. What is the ' +
+                    'atomic number of, hydrogen, plus, George Washington\'s presidency?');
                   assert.deepEqual(gameState, GAME_STATES.PLAYING);
-                  assert.deepEqual(scores.length, 1, 'has one score');
-                  assert(Number.isFinite(scores[0]), 'score is a number');
+                  assert.deepEqual(scores, [311]);
                 }));
           });
         });
